006dee1aa7d8f58f6af3eda6f2eb6722a053d2ed
jash-05 <jash.sheth@gmail.com>
2021-10-21 00:01:46 -0700

    added APIs for customer addresses

--- server/controllers/customer_address.controller.js
+++ server/controllers/customer_address.controller.js
@@ -1,4 +1,5 @@
-const CustomerAddress = require("../models/customer_address.model.js");
+const db = require("../models/db");
+const Customer = db.customers;
 
 // Create and Save a new Address
 exports.create = (req, res) => {
@@ -11,7 +12,6 @@ exports.create = (req, res) => {
 
     // Create object of input variables
     let address_dict = {
-        'customer_ID': req.body.customer_ID,
         'line1': req.body.address_line_1,
         'line2': req.body.address_line_2,
         'city': req.body.city,
@@ -20,77 +20,104 @@ exports.create = (req, res) => {
         'address_type': req.body.address_type
     };
 
-    // Save Favourite in the database
-    CustomerAddress.createAddressForCustomer(address_dict, (err, data) => {  
-      if (err)
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the Customer Address."
+    Customer.updateOne({customer_ID: req.body.customer_ID}, {$push: {addresses: address_dict}}, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Tutorial with id=${req.body.customer_ID}. Maybe Tutorial was not found!`
         });
-      else res.send(data);
+      } else res.send({ message: "Tutorial was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + req.body.customer_ID
+      });
     });
   };
 
-// Retrieve all Favourites from the database.
-exports.findAll = (req, res) => {
-    CustomerAddress.getAddressesForCustomer(req.params.customer_ID, (err, data) => {
-      if (err)
-        res.status(500).send({
-          message: err.message || "Some error occurred while retrieving the customer addresses."
-        });
-      else res.send(data);
-    });
-  };
-
-  exports.getCity = (req, res) => {
-    CustomerAddress.getCityFromCustomerID(req.params.customer_ID, (err, data) => {
-      if (err)
-        res.status(500).send({
-          message: err.message || "Some error occurred while retrieving the city name"
-        });
-      else {
-        res.send(data[0])
-      };
-    });
-  };
-
-
-exports.update = (req, res) => {
+  exports.delete = (req, res) => {
+    // Validate request
     if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
     }
-    CustomerAddress.updateById(
-        req.body, (err, data) => {
-            if (err) {
-                if (err.kind === "not_found") {
-                    res.status(404).send({
-                        message: `Not found restaurant with id: ${req.body.restaurant_ID}`
-                    });
-                } else {
-                    res.status(500).send({
-                        message: `Error updating restaurant with id: ${req.body.restaurant_ID}`
-                    });
-                }
-            } else res.send(data)
-        }
-    );
-};
+
+    Customer.updateOne({customer_ID: req.params.customer_ID}, {$pull: {addresses: {address_type: req.params.address_type}}}, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Tutorial with id=${req.params.customer_ID}. Maybe Tutorial was not found!`
+        });
+      } else res.send({ message: "Tutorial was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + req.params.customer_ID
+      });
+    });
+  };
+
+exports.findAll = (req, res) => {
+    Customer.findOne({customer_ID: req.params.customer_ID}, 'addresses')
+      .then(data => {
+        if (!data)
+          res.status(404).send({ message: "Not found Tutorial with id " + req.params.customerId });
+        else res.send(data);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving Tutorial with id=" + req.params.customerId });
+      });
+  };
+
+// // Retrieve all Favourites from the database.
+// exports.findAll = (req, res) => {
+//     CustomerAddress.getAddressesForCustomer(req.params.customer_ID, (err, data) => {
+//       if (err)
+//         res.status(500).send({
+//           message: err.message || "Some error occurred while retrieving the customer addresses."
+//         });
+//       else res.send(data);
+//     });
+//   };
+
+//   exports.getCity = (req, res) => {
+//     CustomerAddress.getCityFromCustomerID(req.params.customer_ID, (err, data) => {
+//       if (err)
+//         res.status(500).send({
+//           message: err.message || "Some error occurred while retrieving the city name"
+//         });
+//       else {
+//         res.send(data[0])
+//       };
+//     });
+//   };
+
+
+// exports.update = (req, res) => {
+//     if (!req.body) {
+//         res.status(400).send({
+//             message: "Content can not be empty!"
+//         });
+//     }
+//     CustomerAddress.updateById(
+//         req.body, (err, data) => {
+//             if (err) {
+//                 if (err.kind === "not_found") {
+//                     res.status(404).send({
+//                         message: `Not found restaurant with id: ${req.body.restaurant_ID}`
+//                     });
+//                 } else {
+//                     res.status(500).send({
+//                         message: `Error updating restaurant with id: ${req.body.restaurant_ID}`
+//                     });
+//                 }
+//             } else res.send(data)
+//         }
+//     );
+// };
 
 // Delete a Favourite with the specified customer_ID and restaurant_ID in the request
-exports.delete = (req, res) => {
-    CustomerAddress.removeAddressForCustomer(req.params.customer_ID, req.params.address_type,(err, data) => {
-      if (err) {
-        if (err.err_type === "not_found") {
-          res.status(404).send({
-            message: `Could not find address with address type ${req.params.address_type} for customer ${req.params.customer_ID}.`
-          });
-        } else {
-          res.status(500).send({
-            message: `Could not delete address with address type ${req.params.address_type} for customer ${req.params.customer_ID}.`
-          });
-        }
-      } else res.send({ 
-          message: `Address of address type ${req.params.address_type} was deleted successfully for customer ${req.params.customer_ID}!` });
-    });
-  };
+
--- server/routes/customer_address.routes.js
+++ server/routes/customer_address.routes.js
@@ -7,11 +7,11 @@ module.exports = app => {
     // Retrieve all customer addresses
     app.get("/customerAddress/:customer_ID", customer_addresses.findAll);
 
-    // Get city from customer ID
-    app.get("/city/:customer_ID", customer_addresses.getCity);
+    // // Get city from customer ID
+    // app.get("/city/:customer_ID", customer_addresses.getCity);
 
-    // Update customer address
-    app.put('/customerAddress', customer_addresses.update);
+    // // Update customer address
+    // app.put('/customerAddress', customer_addresses.update);
     
     // Delete an address for a customer
     app.delete("/customerAddress/:customer_ID/:address_type", customer_addresses.delete);
