03fdc9aa603a612bf6300ff7a42d76e916d4e838
jash-05 <jash.sheth@gmail.com>
2021-10-21 23:32:17 -0700

    transitioned restaurant address APIs to mongo

--- server/controllers/restaurant_address.controller.js
+++ server/controllers/restaurant_address.controller.js
@@ -1,64 +1,48 @@
-const RestaurantAddress = require("../models/restaurant_address.model.js");
+const db = require("../models/db");
+const Restaurant = db.restaurants;
 
-// Create and Save a new Address
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body) {
-      res.status(400).send({
-        message: "Content can not be empty!"
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  // Create object of input variables
+  let address_dict = {
+      'line1': req.body.address_line_1,
+      'line2': req.body.address_line_2,
+      'city': req.body.city,
+      'state_name': req.body.state,
+      'zipcode': req.body.zip
+  };
+
+  Restaurant.updateOne({restaurant_ID: req.body.restaurant_ID}, {address: address_dict}, { useFindAndModify: false })
+  .then(data => {
+    if (!data) {
+      res.status(404).send({
+        message: `Cannot update Tutorial with id=${req.body.restaurant_ID}. Maybe Tutorial was not found!`
       });
-    }
-
-    // Create object of input variables
-    let address_dict = {
-        'restaurant_ID': req.body.restaurant_ID,
-        'line1': req.body.address_line_1,
-        'line2': req.body.address_line_2,
-        'city': req.body.city,
-        'state_name': req.body.state,
-        'zipcode': req.body.zip
-    };
-
-    // Save Restaurant address in the database
-    RestaurantAddress.createAddressForRestaurant(address_dict, (err, data) => {  
-      if (err)
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the restaurant address."
-        });
-      else res.send(data);
+    } else res.send({ message: "Tutorial was updated successfully." });
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: "Error updating Tutorial with id=" + req.body.restaurant_ID
     });
-  };
+  });
+};
 
-// Retrieve all Favourites from the database.
 exports.findOne = (req, res) => {
-    RestaurantAddress.getAddressForRestaurant(req.params.restaurant_ID, (err, data) => {
-      if (err)
-        res.status(500).send({
-          message: err.message || "Some error occurred while retrieving the restaurant address."
-        });
+  Restaurant.findOne({restaurant_ID: req.params.restaurant_ID}, 'restaurant_ID address')
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Tutorial with id " + req.params.customerId });
       else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Tutorial with id=" + req.params.customerId });
     });
-  };
-
-exports.update = (req, res) => {
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
-    RestaurantAddress.updateById(
-        req.body, (err, data) => {
-            if (err) {
-                if (err.kind === "not_found") {
-                    res.status(404).send({
-                        message: `Not found restaurant with id: ${req.body.restaurant_ID}`
-                    });
-                } else {
-                    res.status(500).send({
-                        message: `Error updating restaurant with id: ${req.body.restaurant_ID}`
-                    });
-                }
-            } else res.send(data)
-        }
-    );
 };
